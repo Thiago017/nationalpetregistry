0b9fca0e25152d58385de48f124f8e7ca502a361
Thiago Pereira <thiago.pereira7ps@gmail.com>
2024-12-02 16:07:22 -0300

    fix(database): alter column gender from CHAR(1) to VARCHAR(1) to resolve bug with incorrect column type

--- src/main/resources/db/migration/V1__Create_users_and_pets_tables.sql
+++ src/main/resources/db/migration/V1__Create_users_and_pets_tables.sql
@@ -4,7 +4,7 @@
 CREATE TABLE users (
   id UUID PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
-  gender CHAR(1) CHECK (gender IN ('M', 'F')),
+  gender VARCHAR(1) CHECK (gender IN ('M', 'F')),
   date_of_birth DATE NOT NULL,
   phone VARCHAR(20),
   address TEXT,
@@ -16,7 +16,7 @@ CREATE TABLE pets (
   id UUID PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   breed VARCHAR(255),
-  gender CHAR(1) CHECK (gender IN ('M', 'F')),
+  gender VARCHAR(1) CHECK (gender IN ('M', 'F')),
   date_of_birth DATE NOT NULL,
   identification_document VARCHAR(15) NOT NULL UNIQUE CHECK (identification_document ~ '^\d{2}\.\d{3}\.\d{3}-\d{3}$'),
   owner_id UUID NOT NULL,
