b0dcdbc6e4fb327cc574cf59a254f9412f8cec6b
Thiago Pereira <thiago.pereira7ps@gmail.com>
2024-12-06 15:44:07 -0300

    refactor: rename class from nationalpetregistryapplication to NationalPetRegistryApplication

--- src/main/java/br/com/thiagopereira/nationalpetregistry/NationalpetregistryApplication.java
+++ src/main/java/br/com/thiagopereira/nationalpetregistry/NationalpetregistryApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NationalpetregistryApplication {
+public class NationalPetRegistryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NationalpetregistryApplication.class, args);
+		SpringApplication.run(NationalPetRegistryApplication.class, args);
 	}
-
 }
